3606ffbb392f8ee73fc24bda520874a7d6fb0376
Pig Fang <g-plane@hotmail.com>
2021-02-17 15:47:45 +0800

    add util for detecting alex texture

---
+++ src/Blessing/Renderer/TextureUtil.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Blessing\Renderer;
+
+use Intervention\Image\ImageManager;
+
+class TextureUtil
+{
+    public function isAlex($imageData): bool
+    {
+        $manager = new ImageManager(['driver' => 'gd']);
+        $image = $manager->make($imageData);
+        $width = $image->width();
+
+        if ($width === $image->height()) {
+            $ratio = $width / 64;
+            for ($x = 46 * $ratio; $x < 48 * $ratio; $x += 1) {
+                for ($y = 52 * $ratio; $y < 64 * $ratio; $y += 1) {
+                    if (!$this->checkPixel($image->pickColor($x, $y))) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected function checkPixel(array $color): bool
+    {
+        return $color[0] === 0 && $color[1] === 0 && $color[2] === 0;
+    }
+}
